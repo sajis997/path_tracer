9493d75e40eb5c8c9bbb6dc0b193abf368e24417
a295697 <sajjadul.islam@volvo.com>
2022-07-09 12:43:06 +0200

    World of spheres added
    World of spheres added

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,23 @@
 
 mod vec; // to use Vec3 in the program, add a reference with mod keyword
 mod ray;
-
-use std::io::{stderr, Write};
+mod hit;
+mod sphere;
 
 use image::{RgbImage,ImageBuffer,Rgb};
 use vec::{Vec3,Color,Point3};
 use ray::Ray;
+use sphere::Sphere;
+use hit::{Hit,World};
 
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin() - center;
-    let a = r.direction().length().powi(2);
-    let half_b = oc.dot(r.direction());
-    let c = oc.length().powi(2) - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-
-    if discriminant < 0.0 {
-        -1.0
+fn ray_color(r: &Ray, world: &World) -> Color {
+    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
+        0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0))
     } else {
-        (-half_b - discriminant.sqrt()) / a
+        let unit_direction = r.direction().normalized();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
     }
-}
-
-fn ray_color(r: &Ray) -> Color {
-
-    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
-
-    if t > 0.0  {
-        let n = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalized();
-        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0)
-    }
-        
-    let unit_direction = r.direction().normalized();
-    let t = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
 fn main() {
@@ -44,8 +28,14 @@ fn main() {
     const IMAGE_WIDTH: u32 = 800;
     const IMAGE_HEIGHT: u32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u32;
     
-
+    //image plane
     let mut buffer: RgbImage = ImageBuffer::new(IMAGE_WIDTH as u32,IMAGE_HEIGHT as u32);
+
+    //world
+    let mut world = World::new();
+    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+    
 
     //camera setup
     let viewport_height = 2.0;
@@ -63,13 +53,13 @@ fn main() {
             
             let ray = Ray::new(origin,lower_left_corner + u * horizontal + v * vertical - origin);
 
-            let color = ray_color(&ray).to_rgb();
+            let color = ray_color(&ray,&world).to_rgb();
 
             *pixel = Rgb(color);
     }
 
      
-    match buffer.save("shadycircle.png") {
+    match buffer.save("world.png") {
         Err(e) => eprintln!("Error writing file {}",e),
         Ok(()) => println!("Saving Done!")
     }    
--- src/sphere.rs
+++ src/sphere.rs
@@ -1,26 +1,54 @@
-use crate::vec::*;
-use crate::ray::*;
+use crate::vec::{Vec3,Point3};
+use crate::ray::Ray;
+use crate::hit::{Hit,HitRecord};
 
 pub struct Sphere {
     center: Point3,
-    radius: f64,
+    radius: f64
 }
 
 impl Sphere {
-    pub fn new(center: Point3, radius: f64) {
+    pub fn new(center: Point3, radius: f64) -> Sphere {
         Sphere {
             center:center,
             radius:radius
         }
     }
+}
 
-    pub fn hit(&self, ray: &Ray) -> bool {
-        let oc = ray.origin() - self.center;
-        let a = ray.direction().dot(ray.direction());
-        let b = 2.0 * oc.dot(ray.direction());
-        let c = oc.dot(oc) - self.radius * self.radius;
-        let discriminant = b * b - 4.0 * a * c;
+impl Hit for Sphere {
+    fn hit(&self, r : &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let oc = r.origin() - self.center;
+        let a = r.direction().length().powi(2);
+        let half_b = oc.dot(r.direction());
+        let c = oc.length().powi(2) - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
+    
+        if discriminant < 0.0 {
+            return None;
+        }
 
-        discriminant > 0.0
+        //find the nearest root that lies in the acceptable range
+        let sqrtd = discriminant.sqrt();
+        let mut root = (-half_b  - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b - sqrtd) / a;
+            if root < t_min || t_max < root {
+                return None;
+            }
+        }
+
+        
+        let mut rec = HitRecord {
+            t: root,
+            p: r.at(root),
+            normal: Vec3::new(0.0,0.0,0.0),
+            front_face: false
+        };
+
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r,outward_normal);
+
+        Some(rec)
     }
 }
