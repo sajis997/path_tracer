a29701dfa865c25ffb8c0c52d1829997dc85fe10
Sajjadul Islam <sajjadul.islam.bd@gmail.com>
2022-09-04 21:53:02 +0200

    functional gamma correction

--- src/vec.rs
+++ src/vec.rs
@@ -67,6 +67,23 @@ impl Vec3 {
         [f(self.e[0]), f(self.e[1]), f(self.e[2])]
     }
 
+    pub fn gamma_correction(&self, samples_per_pixel: u32) -> [u8; 3] {
+        [
+            (256.0
+                * (self.e[0] / (samples_per_pixel as f64))
+                    .sqrt()
+                    .clamp(0.0, 0.999)) as u8,
+            (256.0
+                * (self.e[1] / (samples_per_pixel as f64))
+                    .sqrt()
+                    .clamp(0.0, 0.999)) as u8,
+            (256.0
+                * (self.e[2] / (samples_per_pixel as f64))
+                    .sqrt()
+                    .clamp(0.0, 0.999)) as u8,
+        ]
+    }
+
     pub fn random(r: Range<f64>) -> Vec3 {
         let mut rng = rand::thread_rng();
 
